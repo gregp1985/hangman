02417ae941e24db6ba52c44f0cc88dc9013fa142
gregp1985 <greg@gwphillips.co.uk>
2025-10-27 21:31:38 +0000

    Link to Google Drive, choose difficulty level

--- run.py
+++ run.py
@@ -1,1 +1,53 @@
+import gspread
+from google.oauth2.service_account import Credentials
 
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("words")
+
+def word_picker(level):
+    from numpy import random
+    choice = random.randint(SHEET.worksheet(level).cell(1, 2).value)
+    word_choice = SHEET.worksheet(level).cell(choice, 1).value
+    return word_choice.lower()
+
+
+def choose_level():
+    print("Level 1: Easy\n")
+    print("Level 2: Medium\n")
+    print("Level 3: Hard\n")
+    level_choice = input("Choose a difficulty level (1,2 or 3)")
+    if level_choice == "1":
+        return "easy"
+    elif level_choice == "2":
+        return "medium"
+    elif level_choice == "3":
+        return "hard"
+    else:
+        print("Should be 1, 2 or 3 only!")
+        choose_level()
+
+
+word = word_picker("hard")
+
+def play_game():
+    print("Lets play Hangman!\n")
+    print("     ____")
+    print("     |   |")
+    print("     |   O")
+    print("     |  /|\ ")
+    print("     |  / \ ")
+    print("    /_\ \n")
+
+    level = choose_level()
+    print(level)
+    
+
+
+play_game()
